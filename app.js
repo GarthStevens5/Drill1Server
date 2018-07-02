6784901d3ad6a887ae18aa2e0e75f90f98b2cfb3
garth stevens <gstevens96@gmail.com>
2018-07-02 12:14:45 -0600

    made a top level data component

--- app.js
+++ app.js
@@ -5,27 +5,28 @@ const port = process.env.PORT|| 3000
 
 app.use(cors())
 
-const cohortData = [{
-  id: 1,
-  cohortName: "17-01-WD-DP",
-  cohortCode: "g100",
-  numberOfStudents: 28
-},{
-  id: 2,
-  cohortName: "17-01-DS-GT",
-  cohortCode: "g105",
-  numberOfStudents: 24
-},{
-  id: 3,
-  cohortName: "17-02-WD-PX",
-  cohortCode: "g109",
-  numberOfStudents: 30
-},{
-  id: 4,
-  cohortName: "17-03-WD-BD",
-  cohortCode: "g110",
-  numberOfStudents: 29
-}]
+  const cohortData = { data: [{
+    id: 1,
+    cohortName: "17-01-WD-DP",
+    cohortCode: "g100",
+    numberOfStudents: 28
+  },{
+    id: 2,
+    cohortName: "17-01-DS-GT",
+    cohortCode: "g105",
+    numberOfStudents: 24
+  },{
+    id: 3,
+    cohortName: "17-02-WD-PX",
+    cohortCode: "g109",
+    numberOfStudents: 30
+  },{
+    id: 4,
+    cohortName: "17-03-WD-BD",
+    cohortCode: "g110",
+    numberOfStudents: 29
+  }]
+}
 
 function findById(cohortData, requestedId) {
   return cohortData.find(entry => entry.id === +requestedId)
